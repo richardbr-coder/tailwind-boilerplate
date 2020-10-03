f850c33ed81ba2f4343b17003ee3c2550c16a517
Richard Randell <richrdrandell2@outlook.com>
2020-10-03 21:11:33 -0230

    First commit
    This is first commit to publish the repo!

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules:[
+            {
+                test: /\.css$/,
+                //note!!! Order is from last to first
+                use:[
+                    'style-loader',
+                    {
+                        loader: 'css-loader', options: { importLoaders: 1 }
+                    },
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        watchContentBase: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true
+    }
+}
